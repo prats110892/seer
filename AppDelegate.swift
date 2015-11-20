8757ba842e46e47eed897b474d022ccb2f14768f
Pratik Shah <prats110892@gmail.com>
2015-11-19 22:40:40 -0500

    First Commit
    Working Menu

---
+++ AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  seer
+//
+//  Created by Pratik Shah on 15/11/15.
+//  Copyright © 2015 Pratik Shah. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printQuote:")
+        }
+        
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Select Actor", action: Selector("printQuote:"), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+    
+    func printQuote(sender: AnyObject) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
+}
+
